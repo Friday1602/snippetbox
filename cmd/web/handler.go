d406dae5dd09f6de2642263a63867764d6b71a22
กฤษฎิ์ ศรีวงศ์พรธนา <krit.sr@NBTC.domain>
2024-03-05 10:23:58 +0700

    inject error log to handler

--- cmd/web/handler.go
+++ cmd/web/handler.go
@@ -3,37 +3,37 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+// change the signature of func to be a method against *application.
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
 	}
-// file path 
+	// file path
 	files := []string{
 		"./ui/html/pages/base.tmpl",
 		"./ui/html/pages/home.tmpl",
 		"./ui/html/partials/nav.tmpl",
 	}
-// parse template for home 
+	// parse template for home
 	ts, err := template.ParseFiles(files...)
 	if err != nil {
-		log.Print(err)
+		app.errorLog.Print(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 
 	err = ts.ExecuteTemplate(w, "base", nil)
 	if err != nil {
-		log.Print(err)
+		app.errorLog.Print(err)
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 	}
 }
 
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -44,7 +44,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,6 +7,12 @@ import (
 	"os"
 )
 
+// define application struct to hold the application-wide dependencies.
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	// create command line flag to receive port
 	addr := flag.String("addr", ":4000", "HTTP network address")
@@ -16,6 +22,14 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	// create custom logger for error output to standard error(stderr)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize a new instance of our application struct, containing
+	// the dependencies.
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 	// create a fileserver which serves file out of "./ui/static" directory
 	fileServer := http.FileServer(http.Dir("./ui/static"))
@@ -23,12 +37,12 @@ func main() {
 	// use mux.Handle() to register fileServer as a handler
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 	// route the other application as normal
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
 
 	// create a new http.Server struct
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
 		Handler:  mux,
